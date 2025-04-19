9d016f41db8631d942d0bcf8d7f77a32f9a32518
khalidabdikarim99 <khalidabdikarim9@gmail.com>
2025-04-19 01:18:06 -0700

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from '../data/user';
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+  <h2>About Me</h2>
+  <p>Your custom content</p>
+  <img src={image} alt="I made this" />
+</div>
 }
 
 export default About;
